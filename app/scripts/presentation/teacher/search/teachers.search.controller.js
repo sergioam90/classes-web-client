43ae0799ef19343b26d1da5f75fddc6ceb270947
nicogarcia <nicoalbo90@gmail.com>
2015-08-08 00:09:16 -0300

    Added more filters

--- app/scripts/presentation/teacher/search/teachers.search.controller.js
+++ app/scripts/presentation/teacher/search/teachers.search.controller.js
@@ -5,37 +5,51 @@
         .module('classesClientApp')
         .controller('TeachersSearchController', TeachersSearchController);
 
-    TeachersSearchController.$inject = ['TeacherService', 'Subjects', '$location'];
+    TeachersSearchController.$inject = ['TeacherService', 'Subjects', 'DegreeService', 'CityService', '$location', '$state'];
 
-    function TeachersSearchController(TeacherService, Subjects, $location) {
+    function TeachersSearchController(TeacherService, Subjects, DegreeService, CityService, $location, $state) {
         var vm = this;
 
         vm.subjects = [];
+        vm.degrees = [];
+        vm.cities = CityService.getAllCities();
+
         vm.teachersResult = [];
         vm.search = search;
 
+        vm.viewProfile = viewProfile;
+
         var paramSearch = $location.search();
         vm.searchCriteria = paramSearch ? paramSearch : {};
-
-        vm.selectedSubjects = vm.searchCriteria.subjects;
 
         initialize();
 
         /* Implementation */
 
         function initialize() {
-            getSubjects();
+            loadSubjects();
+
+            loadDegrees();
 
             search();
         }
 
-        function getSubjects() {
+        function loadDegrees() {
+            vm.degrees = [];
+
+            angular.forEach(DegreeService.getAllDegrees(), function (value) {
+                vm.degrees.push({name: value});
+            });
+        }
+
+        function loadSubjects() {
             Subjects.getList().then(function (subjects) {
                 vm.subjects = subjects.plain();
 
-                if (vm.selectedSubjects) {
+                // If there are subjects in url, set them as selected
+                if (vm.searchCriteria && vm.searchCriteria.subjects) {
                     angular.forEach(vm.subjects, function (value) {
-                        value.selected = vm.selectedSubjects.indexOf(value.id) > -1;
+                        value.selected = vm.searchCriteria.subjects.indexOf(value.id) > -1;
                     });
                 }
             });
@@ -43,6 +57,14 @@
 
         function search() {
             var searchCriteria = vm.searchCriteria;
+
+            // Add degrees to search criteria
+            searchCriteria.degrees = [];
+            angular.forEach(vm.degrees, function (value) {
+                if (value.selected) {
+                    searchCriteria.degrees.push(value.name);
+                }
+            });
 
             // Add subjects ids to search criteria
             searchCriteria.subjects = [];
@@ -52,12 +74,19 @@
                 }
             });
 
+            // TODO: Update url with searchCriteria
+
             TeacherService.search(searchCriteria).then(function (page) {
 
                 // TODO: Work with full page object
                 vm.teachersResult = page.content;
             });
         }
+
+        function viewProfile(teacher) {
+            $state.go('teacherProfile', teacher);
+        }
+
     }
 
 })();
