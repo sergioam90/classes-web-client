21dee815088f06e39507804939679a78256a8de7
nicogarcia <nicoalbo90@gmail.com>
2015-09-16 14:53:16 -0300

    Convert birthDate before sending to server (fixes #3)

--- app/scripts/service/user.service.js
+++ app/scripts/service/user.service.js
@@ -16,10 +16,16 @@
         vm.signup = signup;
         vm.confirm = confirm;
 
+        /*
+         TODO: Fix birthDate hack
+         Datepicker works correctly only with 8601
+         */
 
         /* Implementation */
 
         function signup(user) {
+            user.birthDate = moment(user.birthDate).format('YYYY-M-D');
+
             return Users.post(user);
         }
 
@@ -27,15 +33,21 @@
             return Users.one('me').get().then(function (user) {
                 user.birthDate = moment(user.birthDate).format();
 
+                console.log(user.birthDate);
+
                 return user;
             });
         }
 
-        function saveUser(user){
+        function saveUser(user) {
+            user.birthDate = moment(user.birthDate).format('YYYY-M-D');
+
             return Users.one('me').customPUT(user);
         }
 
         function confirm(user) {
+            user.birthDate = moment(user.birthDate).format('YYYY-M-D');
+
             return Users.one('me').one('confirmation').customPOST(user);
         }
 
