c1a48585c885257b78c2647b5b8cd9e88924ce1b
nicogarcia <nicoalbo90@gmail.com>
2015-09-18 11:16:05 -0300

    Finished integration of filter bar (closes #46)

--- app/scripts/directive/search-filter/individual/individual.filter.directive.js
+++ app/scripts/directive/search-filter/individual/individual.filter.directive.js
@@ -8,11 +8,12 @@
 
         function link(scope, element, attributes) {
             scope.toggle = function () {
-                if (scope.state)
+                if (scope.state) {
                     scope.state = undefined;
-                else
+                } else {
                     scope.state = true;
-            }
+                }
+            };
         }
 
         return {
