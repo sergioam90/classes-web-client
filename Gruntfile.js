578576b764732217c2b18db1d72798db431489b0
nicogarcia <nicoalbo90@gmail.com>
2015-09-21 21:16:55 -0300

    Updated api urls to use https

--- Gruntfile.js
+++ Gruntfile.js
@@ -407,14 +407,14 @@ module.exports = function (grunt) {
             development: {
                 constants: {
                     appConfig: {
-                        API_SERVER_URL: 'classes.noip.me:8080'
+                        API_SERVER_URL: 'http://classes.noip.me:8080'
                     }
                 }
             },
             production: {
                 constants: {
                     appConfig: {
-                        API_SERVER_URL: 'classes-bahia.herokuapp.com'
+                        API_SERVER_URL: 'https://api.upclaz.com'
                     }
                 }
             }
