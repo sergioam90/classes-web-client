4dcd2da9a24786b58a0d16e588b2b0f6415dd7c8
nicogarcia <nicoalbo90@gmail.com>
2015-09-23 14:50:06 -0300

    Signup steps design work (#76)

--- app/scripts/directive/signup-steps/signup-steps.directive.js
+++ app/scripts/directive/signup-steps/signup-steps.directive.js
@@ -12,6 +12,8 @@
         function link(scope) {
             scope.isCurrentStep = isCurrentStep;
             scope.setCurrentStep = setCurrentStep;
+            scope.isPastStep = isPastStep;
+            scope.isFutureStep = isFutureStep;
 
             function isCurrentStep(index) {
                 return SignupStepService.getCurrentStep() === index + 1;
@@ -19,6 +21,14 @@
 
             function setCurrentStep(index) {
                 SignupStepService.setCurrentStep(index + 1);
+            }
+
+            function isPastStep(index) {
+                return SignupStepService.getCurrentStep() > index + 1;
+            }
+
+            function isFutureStep(index) {
+                return SignupStepService.getCurrentStep() < index + 1;
             }
         }
 
