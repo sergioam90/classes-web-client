2c58700603c0b32c7096911bd4c93792f859242a
federicojasson <federicojasson@gmail.com>
2015-09-24 11:50:48 -0300

    teacher-card directive (#47)
    TODO:
    - Implement rating and features logic
    - Use directive in the proper views

---
+++ app/scripts/directive/teacher-card/teacher-card.directive.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('classesClientApp')
+        .directive('teacherCard', TeacherCardDirective);
+
+    TeacherCardDirective.$inject = ['$state'];
+
+    function TeacherCardDirective($state) {
+
+        function link(scope) {
+            var teacher = scope.teacher;
+            var isLink = scope.isLink;
+
+            if (isLink) {
+                scope.href = $state.href('root.teacherProfile', {
+                    id: teacher.id
+                });
+            }
+        }
+
+        return {
+            restrict: 'E',
+            scope: {
+                teacher: '=',
+                isLink: '@'
+            },
+            templateUrl: 'scripts/directive/teacher-card/teacher-card.template.html',
+            link: link
+        };
+    }
+})();
